3af8a1a83555f301905fdc4f997be589904540c1
Socvest <82335390+Socvest@users.noreply.github.com>
2022-05-31 03:25:06 +0100

    Add files via upload

---
+++ pretty_notification_box/frontend/src/NicerNotification.tsx
@@ -0,0 +1,58 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
+import "./icon.css"
+import "./style2.css"
+import { style } from 'glamor';
+
+
+
+class NicerNotification extends StreamlitComponentBase {
+  
+  public render = (): ReactNode => {
+    
+    
+    const icon:string = this.props.args['icon']
+    const title:string = this.props.args['title']
+    const textDisplay:any = this.props.args['textDisplay']
+    const externalLink:string = this.props.args['externalLink']
+    const url:any = this.props.args['url']
+    const styles: any = this.props.args['styles'] || {}
+    
+    function removeNotification() { 
+      const el = document.querySelector('.notification-container') as HTMLElement
+      el.style.display='none'; 
+      
+    }
+
+
+  
+    return (
+       <div className="notification-container" {...style(styles['notification-container'])}>
+        <div className="notification-content-container" {...style(styles['notification-content-container'])}>
+          <i className="material-icons" {...style(styles['material-icons'])}>{icon}</i>
+          <span className="title-text-url-container" {...style(styles['title-text-url-container'])}>
+          <div className="title" {...style(styles['title'])}>{title}</div>
+          <div className="notification-text-link-close-container" {...style(styles['notification-text-link-close-container'])}>
+          <div className="notification-text" {...style(styles['notification-text'])}>{textDisplay}</div>
+          <a className="external-link" {...style(styles['external-link'])} href={url} target="_blank">{externalLink}</a>
+          <div className="close-button" {...style(styles['close-button'])} onClick={() => {removeNotification(); this.onClicked();}}>&times;</div></div></span>
+        </div>
+      </div>
+      
+      
+    )
+  };
+
+  private onClicked = (): void => {
+    Streamlit.setComponentValue("")
+  };
+};
+ 
+export default withStreamlitConnection(NicerNotification)
+
+
+
---
+++ pretty_notification_box/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import NicerNotification from "./NicerNotification"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NicerNotification />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
